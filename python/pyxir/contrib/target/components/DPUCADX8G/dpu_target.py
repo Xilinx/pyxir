a127c7fe3ae79aa3b1ca32d92784d5005dab4d04
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-08-11 11:08:26 +0100

    Support external quantizers targeting DPUCADX8G and DPUCZDX8G

--- python/pyxir/contrib/target/components/DPUCADX8G/dpu_target.py
+++ python/pyxir/contrib/target/components/DPUCADX8G/dpu_target.py
@@ -26,11 +26,11 @@ import pyxir
 from pyxir.runtime import base
 from pyxir.runtime.rt_layer import BaseLayer
 from pyxir.graph.transformers import subgraph
-from pyxir.graph.optimization.optimizers.q_optimizer import\
-    QOptimizer
+from pyxir.graph.optimization.optimizers import QOptimizer, ExternalQOptimizer
 from pyxir.quantization.default_quantizer import XGraphDefaultQuantizer
 from pyxir.quantization.mse_quantization.mse_threshold_quantizer import\
     XGraphMSEThresholdQuantizer
+from pyxir.quantization.external_quantizer import ExternalQuantizerTxtOutput
 from pyxir.graph.transformers.layout_transformation_pass import \
     XGraphLayoutTransformationPass
 from pyxir.quantization.decent_quantizer import DECENTQuantizer
@@ -66,6 +66,24 @@ def xgraph_dpu_optimizer(xgraph, target=None, **kwargs):
     optimizer.optimize()
 
     return dpu_xgraph
+
+
+def xgraph_dpu_external_quantizer_optimizer(xgraph, target=None, **kwargs):
+
+    layout_transform_pass = \
+        XGraphLayoutTransformationPass('NHWC', target=target)
+    dpu_xgraph = layout_transform_pass.execute(xgraph, subgraphs_only=False)
+
+    optimizer = ExternalQOptimizer(dpu_xgraph)
+    optimizer.optimize()
+
+    return dpu_xgraph
+
+
+def xgraph_dpu_external_quantizer(xgraph, inputs_func, **kwargs):
+    quantizer = ExternalQuantizerTxtOutput(xgraph, inputs_func, **kwargs)
+    q_xgraph = quantizer.quantize()
+    return q_xgraph
 
 
 def xgraph_dpu_quantizer(xgraph, inputs_func, **kwargs):
