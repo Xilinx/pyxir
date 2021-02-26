56745a2ba2a5f054fb5b13a0c8efb2440edc88e8
pankajdarak-xlnx <35517088+pankajdarak-xlnx@users.noreply.github.com>
2021-02-26 15:07:17 +0530

    Adding DPUCZDX8G SOM target (#26)
    Adding DPUCZDX8G SOM target

--- python/pyxir/contrib/target/DPUCZDX8G_external_quantizer.py
+++ python/pyxir/contrib/target/DPUCZDX8G_external_quantizer.py
@@ -8,6 +8,8 @@ from .components.DPUCZDX8G.zcu102 import xgraph_dpu_zcu102_build_func
 from .components.DPUCZDX8G.zcu102 import xgraph_dpu_zcu102_compiler
 from .components.DPUCZDX8G.zcu104 import xgraph_dpu_zcu104_build_func
 from .components.DPUCZDX8G.zcu104 import xgraph_dpu_zcu104_compiler
+from .components.DPUCZDX8G.som import xgraph_dpu_som_build_func
+from .components.DPUCZDX8G.som import xgraph_dpu_som_compiler
 
 
 # Register target
@@ -39,3 +41,13 @@ pyxir.register_target('DPUCZDX8G-zcu104',
 
 # Register op support
 from .components.DPUCZDX8G import zcu104_op_support
+
+# Register target
+pyxir.register_target('DPUCZDX8G-som',
+                      xgraph_dpu_external_quantizer_optimizer,
+                      xgraph_dpu_external_quantizer,
+                      xgraph_dpu_som_compiler,
+                      xgraph_dpu_som_build_func)
+
+# Register op support
+from .components.DPUCZDX8G import som_op_support
