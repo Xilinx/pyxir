986c53f5d87d6175518fd664b1937a11f5647565
Jorn Tuyls <jornt@xilinx.com>
2021-04-23 07:01:21 -0700

    Version bump to v0.2.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ from setuptools.command.build_ext import build_ext
 
 from pathlib import Path
 
-__version__ = '0.1.7'
+__version__ = '0.2.0'
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 
