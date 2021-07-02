d6aa028f9d99b836f54643166aa8c466f0f8af0a
anilm <anilm@xilinx.com>
2021-07-02 03:38:13 -0700

    Add correct registration function

--- python/pyxir/contrib/target/DPUCAHX8L_external_quantizer.py
+++ python/pyxir/contrib/target/DPUCAHX8L_external_quantizer.py
@@ -2,28 +2,17 @@ import pyxir
 
 from .components.DPUCZDX8G.common import xgraph_dpu_external_quantizer
 from .components.DPUCZDX8G.common import xgraph_dpu_external_quantizer_optimizer
-from .components.DPUCZDX8G.u50 import xgraph_dpu_u50_build_func
-from .components.DPUCZDX8G.u50 import xgraph_dpu_u50_compiler
-from .components.DPUCZDX8G.u280 import xgraph_dpu_u280_build_func
-from .components.DPUCZDX8G.u280 import xgraph_dpu_u280_compiler
+from .components.DPUCZDX8G.dpucahx8l import xgraph_dpu_build_func
+from .components.DPUCZDX8G.dpucahx8l import xgraph_dpu_compiler
+
 
 
 # Register target
-pyxir.register_target('DPUCAHX8L-u50',
+pyxir.register_target('DPUCAHX8L',
                       xgraph_dpu_external_quantizer_optimizer,
                       xgraph_dpu_external_quantizer,
-                      xgraph_dpu_u50_compiler,
-                      xgraph_dpu_u50_build_func)
+                      xgraph_dpu_compiler,
+                      xgraph_dpu_build_func)
 
 # Register op support
-from .components.DPUCAHX8L import u50_op_support
-
-# Register U280 target
-pyxir.register_target('DPUCAHX8L-u280',
-                      xgraph_dpu_external_quantizer_optimizer,
-                      xgraph_dpu_external_quantizer,
-                      xgraph_dpu_u280_compiler,
-                      xgraph_dpu_u280_build_func)
-
-# Register op support
-from .components.DPUCAHX8L import u280_op_support
+from .components.DPUCAHX8L import op_support
