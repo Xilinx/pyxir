f23f40f1d596e9c5c03616fcc24f17aabb592fc3
jornt-xilinx <65705538+jornt-xilinx@users.noreply.github.com>
2022-01-03 22:38:55 +0000

    Update u50lv.py

--- python/pyxir/contrib/target/components/DPUCAHX8H/u50lv.py
+++ python/pyxir/contrib/target/components/DPUCAHX8H/u50lv.py
@@ -29,7 +29,6 @@ logger = logging.getLogger('pyxir')
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 def xgraph_dpu_u50lv_build_func(xgraph, work_dir=os.getcwd(), **kwargs):
 
-    # TODO here or in optimizer, both?
     # DPU layers are in NHWC format because of the tensorflow
     #   intemediate structure we use to communicate with
     #   DECENT/DNNC
