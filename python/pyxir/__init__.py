8c4dd545a693078a5b61a0db7aa7358073596804
Jorn Tuyls <jornt@xilinx.com>
2022-01-10 21:09:50 -0800

    Version bump to v0.3.3

--- python/pyxir/__init__.py
+++ python/pyxir/__init__.py
@@ -26,7 +26,7 @@ from pyxir.targets.cpu import build_for_cpu_execution,\
 from .graph.xop_registry import XOpRegistry, xop_register_op_layout_transform,\
     xop_register_op_transpose_transform
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 
 device_r = TargetRegistry()
