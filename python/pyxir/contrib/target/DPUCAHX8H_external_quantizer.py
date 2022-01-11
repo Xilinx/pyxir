fa76ac9919eb2163a696743763f0838a2edc6c88
anilmartha <anilmartha963@gmail.com>
2022-01-11 11:12:07 +0530

    Add U50LV and U55C targets  (#81)
    * Add u55c and u55c_dwc targets
    
    * register u55 targets in runtime
    
    * Remove u55c target
    
    * Add u50LV target
    
    * Register DPUCAHX8H-u55c_dwc in runtime
    
    * Update u50lv.py
    
    * Update u50lv_dwc.py
    
    * Update u55c_dwc.py
    
    * Set avg_pool_min_kernel to 1 and fix relu6 op support
    
    * Add upscale opeartor support with scale, input channel,etc.
    
    * Add upsample test case
    
    Co-authored-by: jornt-xilinx <65705538+jornt-xilinx@users.noreply.github.com>

--- python/pyxir/contrib/target/DPUCAHX8H_external_quantizer.py
+++ python/pyxir/contrib/target/DPUCAHX8H_external_quantizer.py
@@ -2,10 +2,16 @@ import pyxir
 
 from .components.DPUCZDX8G.external_quantizer_tools import xgraph_dpu_external_quantizer
 from .components.DPUCZDX8G.external_quantizer_tools import xgraph_dpu_external_quantizer_optimizer
-from .components.DPUCZDX8G.u50 import xgraph_dpu_u50_build_func
-from .components.DPUCZDX8G.u50 import xgraph_dpu_u50_compiler
-from .components.DPUCZDX8G.u280 import xgraph_dpu_u280_build_func
-from .components.DPUCZDX8G.u280 import xgraph_dpu_u280_compiler
+from .components.DPUCAHX8H.u50 import xgraph_dpu_u50_build_func
+from .components.DPUCAHX8H.u50 import xgraph_dpu_u50_compiler
+from .components.DPUCAHX8H.u50lv import xgraph_dpu_u50lv_build_func
+from .components.DPUCAHX8H.u50lv import xgraph_dpu_u50lv_compiler
+from .components.DPUCAHX8H.u50lv_dwc import xgraph_dpu_u50lv_dwc_build_func
+from .components.DPUCAHX8H.u50lv_dwc import xgraph_dpu_u50lv_dwc_compiler
+from .components.DPUCAHX8H.u55c_dwc import xgraph_dpu_u55c_dwc_build_func
+from .components.DPUCAHX8H.u55c_dwc import xgraph_dpu_u55c_dwc_compiler
+from .components.DPUCAHX8H.u280 import xgraph_dpu_u280_build_func
+from .components.DPUCAHX8H.u280 import xgraph_dpu_u280_compiler
 
 
 # Register target
@@ -17,6 +23,38 @@ pyxir.register_target('DPUCAHX8H-u50',
 
 # Register op support
 from .components.DPUCAHX8H import u50_op_support
+
+# Register target
+pyxir.register_target('DPUCAHX8H-u50lv',
+                      xgraph_dpu_external_quantizer_optimizer,
+                      xgraph_dpu_external_quantizer,
+                      xgraph_dpu_u50lv_compiler,
+                      xgraph_dpu_u50lv_build_func)
+
+# Register op support
+from .components.DPUCAHX8H import u50lv_op_support
+
+# Register op support
+from .components.DPUCAHX8H import u50lv_dwc_op_support
+
+# Register target
+pyxir.register_target('DPUCAHX8H-u50lv_dwc',
+                      xgraph_dpu_external_quantizer_optimizer,
+                      xgraph_dpu_external_quantizer,
+                      xgraph_dpu_u50lv_dwc_compiler,
+                      xgraph_dpu_u50lv_dwc_build_func)
+
+# Register target
+pyxir.register_target('DPUCAHX8H-u55c_dwc',
+                      xgraph_dpu_external_quantizer_optimizer,
+                      xgraph_dpu_external_quantizer,
+                      xgraph_dpu_u55c_dwc_compiler,
+                      xgraph_dpu_u55c_dwc_build_func)
+
+
+# Register op support
+from .components.DPUCAHX8H import u55c_dwc__op_support
+
 
 # Register U280 target
 pyxir.register_target('DPUCAHX8H-u280',
